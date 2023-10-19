4aaa676e1e0b56b0e7e9e1d32a99571533f54960
CarlosLeyvaAyala <64770846+CarlosLeyvaAyala@users.noreply.github.com>
2023-10-19 12:38:06 -0600

    Add properties

--- OkApplyCancel.xaml.cs
+++ OkApplyCancel.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace DM_WpfControls;
 /// <summary>
@@ -8,9 +9,11 @@ namespace DM_WpfControls;
 public partial class OkApplyCancel : UserControl {
   public event RoutedEventHandler? OkClick;
   public event RoutedEventHandler? ApplyClick;
+  public event RoutedEventHandler? CancelClick;
   public OkApplyCancel() => InitializeComponent();
   private void OnOk(object sender, RoutedEventArgs e) => OkClick?.Invoke(this, e);
   private void OnApply(object sender, RoutedEventArgs e) => ApplyClick?.Invoke(this, e);
+  private void OnCancel(object sender, RoutedEventArgs e) => CancelClick?.Invoke(this, e);
 
   #region Property : ShowApply
   public bool ShowApply {
@@ -29,4 +32,20 @@ public partial class OkApplyCancel : UserControl {
   void ShowApplyPropertyChanged(bool value) => btnApply.Visibility = value ? Visibility.Visible : Visibility.Collapsed;
   #endregion
 
+  #region Property : OkBackground
+  public Brush OkBackground {
+    get { return (Brush)GetValue(OkBackgroundProperty); }
+    set { SetValue(OkBackgroundProperty, value); }
+  }
+
+  public static readonly DependencyProperty OkBackgroundProperty =
+      DependencyProperty.Register(
+        nameof(OkBackground),
+        typeof(Brush),
+        typeof(OkApplyCancel),
+        new PropertyMetadata(default(Brush), OkBackgroundPropertyChanged));
+  static void OkBackgroundPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) =>
+    ((OkApplyCancel)d).OkBackgroundPropertyChanged((Brush)e.NewValue);
+  void OkBackgroundPropertyChanged(Brush value) => btnOk.Background = value;
+  #endregion
 }
