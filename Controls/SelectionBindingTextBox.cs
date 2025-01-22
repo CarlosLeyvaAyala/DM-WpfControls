97004ae9bc816681b2008f963766f7f6831e9935
CarlosLeyvaAyala <64770846+CarlosLeyvaAyala@users.noreply.github.com>
2025-01-22 06:49:02 -0600

    Add stuff

---
+++ Controls/SelectionBindingTextBox.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DM_WpfControls.Controls;
+
+public class SelectionBindingTextBox : TextBox {
+    public static readonly DependencyProperty BindableSelectionStartProperty =
+        DependencyProperty.Register(
+        "BindableSelectionStart",
+        typeof(int),
+        typeof(SelectionBindingTextBox),
+        new PropertyMetadata(OnBindableSelectionStartChanged));
+
+    public static readonly DependencyProperty BindableSelectionLengthProperty =
+        DependencyProperty.Register(
+        "BindableSelectionLength",
+        typeof(int),
+        typeof(SelectionBindingTextBox),
+        new PropertyMetadata(OnBindableSelectionLengthChanged));
+
+    public static readonly DependencyProperty BindableVerticalOffsetProperty =
+        DependencyProperty.Register(
+        "BindableVerticalOffset",
+        typeof(double),
+        typeof(SelectionBindingTextBox),
+        new PropertyMetadata(OnBindableVerticalOffsetChanged));
+
+    public SelectionBindingTextBox() : base() => SelectionChanged += OnSelectionChanged;
+
+    public int BindableSelectionStart {
+        get => (int)GetValue(BindableSelectionStartProperty);
+        set => SetValue(BindableSelectionStartProperty, value);
+    }
+
+    public int BindableSelectionLength {
+        get => (int)GetValue(BindableSelectionLengthProperty);
+        set => SetValue(BindableSelectionLengthProperty, value);
+    }
+
+    public double BindableVerticalOffset {
+        get => (double)GetValue(BindableVerticalOffsetProperty);
+        set => SetValue(BindableVerticalOffsetProperty, value);
+    }
+
+    private static void OnBindableVerticalOffsetChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args) {
+        Console.WriteLine("OnBindableVerticalOffsetChanged");
+        if (dependencyObject is not SelectionBindingTextBox textBox) return;
+        textBox.ScrollToVerticalOffset((double)args.NewValue);
+        Console.WriteLine($"New value {(double)args.NewValue}");
+    }
+
+    private static void OnBindableSelectionStartChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args) {
+        if (dependencyObject is not SelectionBindingTextBox textBox) return;
+
+        int newValue = (int)args.NewValue;
+        textBox.SelectionStart = newValue;
+    }
+
+    private static void OnBindableSelectionLengthChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args) {
+        if (dependencyObject is not SelectionBindingTextBox textBox) return;
+
+        int newValue = (int)args.NewValue;
+        textBox.SelectionLength = newValue;
+    }
+
+    private void OnSelectionChanged(object sender, RoutedEventArgs e) {
+        BindableSelectionStart = SelectionStart;
+        BindableSelectionLength = SelectionLength;
+        BindableVerticalOffset = VerticalOffset;
+    }
+}
---
+++ Converters/MultiCommandArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DM_WpfControls.Converters;
+public class MultiCommandArgs : IMultiValueConverter {
+    public object Convert(object[] values, Type _, object __, CultureInfo ___) => values.Clone();
+    public object[] ConvertBack(
+        object value,
+        Type[] targetTypes,
+        object parameter,
+        CultureInfo culture) => throw new NotImplementedException();
+}
+
